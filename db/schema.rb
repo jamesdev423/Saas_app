2b503420b6978326898eccbffa5a446edaa1702c
James Sprague <jameson4913@gmail.com>
2016-10-11 10:57:55 -0700

    Added names to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008170911) do
+ActiveRecord::Schema.define(version: 20161011175614) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -111,6 +111,8 @@ ActiveRecord::Schema.define(version: 20161008170911) do
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
     t.string   "stripe_customer_token"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
