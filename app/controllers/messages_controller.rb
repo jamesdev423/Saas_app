c8fca12c918c5e21ddb9a8569f0752d254b62c73
James Sprague <jameson4913@gmail.com>
2016-10-11 16:44:04 -0700

    Added routes and views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,21 @@
 class MessagesController < ApplicationController
-	before_action :set_conversation
+  before_action :set_conversation
 
-	def new
-	end
+  def create
+    current_user.reply_to_conversation(
+      @conversation,
+      params[:mailboxer_message][:body],
+      nil,
+      true,
+      true,
+      params[:mailboxer_message][:attachment]
+    )
+    redirect_to conversation_path(@conversation)
+  end
 
-	def create
-		receipt = current_user.reply_to_conversation(@conversation, params[:body])
-		redirect_to conversation_path(receipt.conversation)
-	end
+  private
 
-	private
-		def set_conversation
-			@conversation = current_user.mailbox.conversations.find(params[:conversation_id])
-		end
+    def set_conversation
+      @conversation = current_user.mailbox.conversations.find(params[:conversation_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,18 @@ Rails.application.routes.draw do
   	resources :users do
     		resource :profile
       end
+
       resources :conversations do
-      	resources :messages
+          resources :messages
+
+          collection do
+              get :inbox
+              get :all, action: :index
+              get :sent
+              get :trash
+          end
       end
+
 	resources :contacts
 	get '/about' => 'pages#about'
 	root 'pages#home'
