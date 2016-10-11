3cd1fd4b5be16823efa40e1eab524746522e8822
James Sprague <jameson4913@gmail.com>
2016-10-11 10:48:20 -0700

    Adding routes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
 class MessagesController < ApplicationController
 	before_action :set_conversation
 
+	def new
+	end
+
 	def create
 		receipt = current_user.reply_to_conversation(@conversation, params[:body])
 		redirect_to conversation_path(receipt.conversation)
