394fa44111258277e8805c2d47866c59e72566f4
James Sprague <jameson4913@gmail.com>
2016-10-17 12:19:55 -0700

    Configured mailboxer to find users by first and last name

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,8 +1,8 @@
 module MessagesHelper
   def recipients_options
     s = ''
-    User.all.each do |user|
-      s << "<option value='#{user.id}' data-img-src='#{gravatar_image_url(user.email, size: 50)}'>#{user.name}</option>"
+    User.all.each do |user| #need to add code so users can only send messages to users with plan 2
+      s << "<option value='#{user.id}' data-img-src='#{gravatar_image_url(user.email, size: 50)}'>#{user.first_name} #{user.last_name}</option>"
     end
     s.html_safe
   end
